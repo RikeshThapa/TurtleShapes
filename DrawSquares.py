4ba8e9539f0cac346d1cdde69309b90a796e08f5
Rikesh Thapa <rthapa@u.rochester.edu>
2016-12-15 08:09:17 -0800

    adding some basic shape change functionality and basic error management for the shapes

--- DrawSquares.py
+++ DrawSquares.py
@@ -1,15 +1,20 @@
 import turtle
 
-def draw_square():
+def draw_square(format = "classic"):
     window = turtle.Screen()
     window.bgcolor("red")
     i = 0
     brad = turtle.Turtle()
+    if format not in ["arrow", "turtle", "circle", "square", "triangle", "classic"]:
+        brad.shape("classic")
+    else:
+        brad.shape(format)
+    
     while i<4:
         brad.forward(100)
         brad.right(90)
         i+=1
     window.exitonclick()
 
-draw_square()
+draw_square("turtle")
     
