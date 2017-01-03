54a71f9ca4cf65048e2a991d5aa57aad3703169e
Rikesh Thapa <rthapa@u.rochester.edu>
2017-01-02 21:48:46 -0800

    adding a triangle drawer

--- DrawSquares.py
+++ DrawSquares.py
@@ -1,8 +1,11 @@
 import turtle
 
-def draw_square(format = "classic", color = "black", speed = 6):
+def initialize_window(color = "white"):
     window = turtle.Screen()
-    window.bgcolor("red")
+    window.bgcolor(color)
+    window.exitonclick()
+
+def draw_square(format = "classic", color = "black", speed = 6):
     i = 0
     brad = turtle.Turtle()
     brad.color(color)
@@ -11,12 +14,22 @@ def draw_square(format = "classic", color = "black", speed = 6):
         brad.shape("classic")
     else:
         brad.shape(format)
-    
     while i<4:
         brad.forward(100)
         brad.right(90)
         i+=1
-    window.exitonclick()
 
-draw_square("turtle", "blue", 1)
+def draw_circle(format = "classic", color="blue", speed = 6):
+    chris = turtle.Turtle()
+    chris.color(color)
+    chris.speed(speed)
+    if format not in ["arrow", "turtle", "circle", "square", "triangle", "classic"]:
+        chris.shape("classic")
+    else:
+        chris.shape(format)
+    chris.circle(120)
+
+initialize_window()
+draw_square("turtle", "red", 1)
+draw_circle()
     
